56ff4a7bf51833399b7c08bfbff423a95097ddd4
Luis Del Arco <i317061686@gmail.com>
2023-03-01 10:27:17 -0600

    dev: Add users

--- bioconductor/assets/map_existing_users.sh
+++ bioconductor/assets/map_existing_users.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/csh
+#Add genomx group
 groupadd -g `getent group genomx | cut -d: -f3` genomx
+
+#Users 
+pwd=$(pwd)
+foreach line (`cat pwd/users`)
+  set split = ($line:as/,/ /)
+  set name = $split[1] 
+  set id = $split[2]
+  set password = $split[3]
+
+  useradd $name -u $id -M -N --groups genomx #--comment "Paulina Baca <paulinabacap@gmail.com>"
+  echo $name:$password | chpasswd
