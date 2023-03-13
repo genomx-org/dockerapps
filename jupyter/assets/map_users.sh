3992ff9ed75cf6db81ac6381b3acedc94f3a160f
Fernando Rivas <fernando@genomx.be>
2023-03-13 10:17:33 -0600

    dev: add users to jupyter

--- jupyter/assets/map_users.sh
+++ jupyter/assets/map_users.sh
@@ -6,12 +6,9 @@ echo "Getting users at /tmp/users"
 echo ---
 while IFS="," read -r username id password comment
 do
-  echo "username: $username"
-  echo "id: $id"
-  echo "password: $password"
-  echo "comment: $comment"
-  # add and change pwd
-  useradd "$username" -u "$id" --no-create-home --groups genomx --comment "$comment"
+  echo "Creating user. username: $username"
+  groupadd -g "$id" "$username"
+  useradd "$username" -u "$id" -g "$id" --no-create-home --groups genomx --comment "$comment"
   echo "$username:$password" | chpasswd
 done < /tmp/users
   
