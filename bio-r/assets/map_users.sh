3576369af3e1077597209424122ae6476c333c90
LuisErnesto21 <i317061686@gmail.com>
2023-04-25 16:55:53 -0600

    Guests users and new directory structure

--- bio-r/assets/map_users.sh
+++ bio-r/assets/map_users.sh
@@ -1,6 +1,12 @@
 #!/bin/bash  
+# Groups
+groupadd -g 1100 genomx
+groupadd -g 2000 public
 
-# Users
+#gen-->rstudio
+usermod -u 1101 -a -G genomx,public rstudio
+groupmod -g 1101 rstudio
+# Users-genomx,guests
 echo ---
 echo "Getting users at /tmp/users"
 echo ---
@@ -8,7 +14,7 @@ while IFS="," read -r username id password comment
 do
   echo "Creating user. username: $username"
   groupadd -g "$id" "$username"
-  useradd "$username" -u "$id" -g "$id" --no-create-home --groups genomx --comment "$comment"
+  useradd "$username" -u "$id" -g "$id" --no-create-home --groups genomx,public --comment "$comment"
   echo "$username:$password" | chpasswd
 done < /tmp/users
-  
+
